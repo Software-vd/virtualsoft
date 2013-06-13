500c260d1b093cfaa7efa86b5c9ee971f05aa69b
veronica <vero141930@hotmail.com>
2013-06-13 09:40:08 -0500

    vero

--- app/models/tipo.rb
+++ app/models/tipo.rb
@@ -1,7 +1,7 @@
 class Tipo < ActiveRecord::Base
   attr_accessible :Descripcion
   
-  validates :Descripcion :presence => true, 
+  validates :Descripcion, :presence => true, 
   :length => { :maximum => 80 }
 
   belongs_to :producto
